70cebf8c7750539b4c858c9f4abec6b45df22681
debutler266 <32465329+debutler266@users.noreply.github.com>
2021-10-26 18:06:42 -0400

    added responsivness for header/banner section

--- main.js
+++ main.js
@@ -9,8 +9,12 @@ sideNav.style.right = '-250px';
 menuBtn.onclick = function(){
   if(sideNav.style.right == '-250px'){
     sideNav.style.right = '0';
+    menu.src = 'img/close.png';
   }
   else{
     sideNav.style.right = '-250px';
+    //displays 'close' icon accordingly, closes side menu when clicked
+    menu.src = 'img/menu.png';
+
   }
 }
